eb3c308f5fe1f4e44deb084d5a9d058f266905fe
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-09-07 23:53:46 -0300

    Merge de Cambios

---
+++ Grupo 5 TP TADP/SRC/PosibleIdea.rb
@@ -0,0 +1,74 @@
+class MiSuite
+
+  def initialize()
+    @hola = 2
+    @jaja = 1
+  end
+
+  # TODO Iria a Mixin de Suites
+  def self.ser(argumento)
+    # Tiene varios Puts para Debug y Pruebas
+    puts
+    puts argumento.class
+
+    # Discrimino Comportamiento segun Sea un Proc o un Objeto
+    if argumento.is_a? Proc
+      # FIXME Borrar Puts para debug
+      puts "Es un bloque"
+      return proc { |param|  true.equal?(argumento.call param) }
+    else
+      puts "No es un bloque, es un Objeto"
+      return proc  {|param|  param.equal? argumento }
+    end
+
+  end
+
+  # TODO Hay que Decidir pasarlo a Object o Class o donde sea
+  def deberia(proc)
+    #FIXME Put para Debug, iria sin los Put
+    puts proc.call self
+    puts puts
+  end
+
+  # TODO Iria a Mixin de Suites
+  def self.mayor_a(parametro)
+    return proc { |var|  var > parametro }
+  end
+
+  def self.menor_a(parametro)
+    return proc { |var|  var < parametro }
+  end
+
+  def self.uno_de_estos(lista)
+    return proc { |var| lista.include? var }
+  end
+
+  def method_missing(symbol, *args)
+    if symbol.to_s[0..3] == "ser_"
+      mensaje = symbol.to_s[4..(symbol.to_s.length-1)] + "?"
+      return proc {|var| var.send(mensaje.to_sym)}
+    end
+    super
+  end
+
+  # TODO Iria a Mixin de Suites
+  def self.entender(symbol)
+    return proc { |var|  var.respond_to? symbol }
+  end
+
+  #Ejemplo Codigo para Correr
+  mitest = MiSuite.new
+
+  # Explora porque comparamos 2 cosas distintas
+  # mitest.deberia ser mayor_a 6
+
+  # Da True
+  mitest.deberia ser mitest
+
+  # Da False
+  mitest.deberia ser 9
+
+  # Da True
+  mitest.deberia entender :deberia
+
+end
