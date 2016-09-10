f23311c9b51a7e7f40355315af2421d127631907
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-09-10 17:37:36 -0300

    Primer Test Real =)

--- Grupo 5 TP TADP/SPEC/tadspec_spec.rb
+++ Grupo 5 TP TADP/SPEC/tadspec_spec.rb
@@ -1,2 +1,19 @@
 require "rspec"
 require_relative "../SRC/TADsPec"
+
+describe 'Framework de Testing' do
+  # Suite para probar
+  class SuiteDePrueba
+
+    def testear_que_funciona
+      true
+    end
+
+  end
+
+
+
+  it 'No Deberia dar ninguna Excepcion al Correr la Suite Entera' do
+    expect { TADsPec.testear SuiteDePrueba }.to_not raise_error
+  end
+end
