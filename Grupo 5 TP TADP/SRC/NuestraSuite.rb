8b02917e8c12d66b1d1f87eb612e9cdabea2a7ea
Juan Larosa <juanlarosa96@hotmail.com>
2016-09-12 18:25:28 -0300

    ARmo module de NuestraSuite

--- Grupo 5 TP TADP/SRC/NuestraSuite.rb
+++ Grupo 5 TP TADP/SRC/NuestraSuite.rb
@@ -1,4 +1,54 @@
 module NuestraSuite
   # TODO Pendiente
+
+  def ser(argumento)
+
+    # Discrimino Comportamiento segun Sea un Proc o un Objeto
+    if argumento.respond_to? :call
+      return proc   { |param|   resultado = argumento.call param
+      raise FalloTest.new("Se esperaba 'True' pero llego '#{resultado.to_s}'") unless true.equal?(resultado)
+      }
+    else
+      return proc   {|param|
+        raise FalloTest.new("Se esperaba '#{param.to_s}' pero llego '#{argumento.to_s}'") unless param.equal? argumento
+      }
+    end
+
+  end
+
+  def mayor_a(parametro)
+    return proc { |var|  var > parametro }
+  end
+
+  def menor_a(parametro)
+    return proc { |var|  var < parametro }
+  end
+
+  def uno_de_estos(lista)
+    return proc { |var| lista.include? var }
+  end
+
+  def method_missing(symbol, *args)
+    if symbol.to_s[0..3] == "ser_"
+      mensaje = symbol.to_s[4..(symbol.to_s.length-1)] + "?"
+      return proc {|var| var.send(mensaje.to_sym)}
+    end
+    super
+  end
+
+  def entender(symbol)
+    return proc { |var|  var.respond_to? symbol }
+  end
+
+  def explotar_con(exception)
+    return proc {|bloque|
+      begin
+        bloque.call
+        return false
+      rescue Exception => e
+        return e.is_a? exception
+      end
+    }
+  end
 end
 
--- Grupo 5 TP TADP/SRC/PosibleIdea.rb
+++ Grupo 5 TP TADP/SRC/PosibleIdea.rb
@@ -7,91 +7,9 @@ class MiSuite
     @jaja = 1
   end
 
-  # TODO Iria a Mixin de Suites
-  def self.ser(argumento)
-    # Tiene varios Puts para Debug y Pruebas
-    puts
-    puts argumento.class
-
-    # Discrimino Comportamiento segun Sea un Proc o un Objeto
-    if argumento.respond_to? :call
-      # FIXME Borrar Puts para debug
-      puts "Es un bloque"
-      return proc   { |param|   resultado = argumento.call param
-                                raise FalloTest.new("Se esperaba 'True' pero llego '#{resultado.to_s}'") unless true.equal?(resultado)
-                    }
-    else
-      puts "No es un bloque, es un Objeto"
-      return proc   {|param|
-                              raise FalloTest.new("Se esperaba '#{param.to_s}' pero llego '#{argumento.to_s}'") unless param.equal? argumento
-                    }
-    end
-
-  end
-
   # TODO Hay que Decidir pasarlo a Object o Class o donde sea
   def deberia(proc)
     resultado = proc.call self
-
-    #FIXME Put para Debug, iria sin los Put
-    puts resultado
-    puts puts
   end
-
-  # TODO Iria a Mixin de Suites
-  def self.mayor_a(parametro)
-    return proc { |var|  var > parametro }
-  end
-
-  def self.menor_a(parametro)
-    return proc { |var|  var < parametro }
-  end
-
-  def self.uno_de_estos(lista)
-    return proc { |var| lista.include? var }
-  end
-
-  def method_missing(symbol, *args)
-    if symbol.to_s[0..3] == "ser_"
-      mensaje = symbol.to_s[4..(symbol.to_s.length-1)] + "?"
-      return proc {|var| var.send(mensaje.to_sym)}
-    end
-    super
-  end
-
-  # TODO Iria a Mixin de Suites
-  def self.entender(symbol)
-    return proc { |var|  var.respond_to? symbol }
-  end
-
-  def self.explotar_con(exception)
-    return proc {|bloque|
-      begin
-        bloque.call
-        return false
-
-      rescue Exception => e
-        return e.is_a? exception
-      end
-    }
-  end
-
-  #Ejemplo Codigo para Correr
-  mitest = MiSuite.new
-
-  # Explota porque comparamos 2 cosas distintas
-  # mitest.deberia ser mayor_a 6
-
-  # Da True
-  mitest.deberia ser mitest
-
-  # Da True
-  mitest.deberia entender :deberia
-
-  # Da False (Excepcion)
-  mitest.deberia ser proc { false }
-
-  # Da False (Excepcion)
-  mitest.deberia ser 9
 
 end
