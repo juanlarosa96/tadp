4ed943c5013121d29ef7c80965ffc8fe3a90ba97
JavierZ <javier.zolotarchuk@despegar.com>
2016-09-18 22:39:15 -0300

    tener implementacion terminada

--- Grupo 5 TP TADP/SPEC/tadspec_spec.rb
+++ Grupo 5 TP TADP/SPEC/tadspec_spec.rb
@@ -3,6 +3,11 @@ require_relative "../SRC/TADsPec"
 
 describe 'Framework de Testing' do
   # Suite para probar
+
+  class Persona
+    @onda = true
+  end
+
   class SuiteDePrueba
 
     def testear_que_funciona
@@ -10,6 +15,11 @@ describe 'Framework de Testing' do
       true.deberia ser true
     end
 
+
+    def testear_que_funciona_el_tener
+    javee = Persona.new
+    javee.tener_onda true
+    end
   end
 
   class ClaseNoSuite
@@ -39,6 +49,10 @@ describe 'Framework de Testing' do
 
   it 'Deberia Ejecutar Bien al Correr Todas las Suites del Contexto' do
     expect(TADsPec.testear_contexto).to eq(EJECUCION_CORRECTA)
+  end
+
+  it 'asdadadsdaddads' do
+    expect { TADsPec.testear SuiteDePrueba, :testear_que_funciona_el_tener}.to_not raise_error
   end
 
 end
--- Grupo 5 TP TADP/SRC/NuestraSuite.rb
+++ Grupo 5 TP TADP/SRC/NuestraSuite.rb
@@ -5,11 +5,11 @@ module NuestraSuite
 
     # Discrimino Comportamiento segun Sea un Proc o un Objeto
     if argumento.respond_to? :call
-      return proc   { |param|   resultado = argumento.call param
+      return proc { |param| resultado = argumento.call param
       raise FalloTest.new("Se esperaba 'True' pero llego '#{resultado.to_s}'") unless true.equal?(resultado)
       }
     else
-      return proc   {|param|
+      return proc { |param|
         raise FalloTest.new("Se esperaba '#{param.to_s}' pero llego '#{argumento.to_s}'") unless param.equal? argumento
       }
     end
@@ -17,10 +17,10 @@ module NuestraSuite
   end
 
   def tener_con_val(param, val)
-    mensaje = ("@" + param).to_sym
-    if (instance_variable_defined? mensaje) and (instance_variable_get(mensaje).equal? val)
+    atributo = ("@" + param).to_sym
+    if (instance_variable_defined? atributo) and (instance_variable_get(atributo).equal? val)
       #Aca meteria el proc que devuelve
-    elsif instance_variable_defined? mensaje #Desde aca verifico x cual de los motivos rompio --> MEJORAR
+    elsif instance_variable_defined? atributo #Desde aca verifico x cual de los motivos rompio --> MEJORAR
       "rompi por val"
     else
       "rompi por msj"
@@ -28,11 +28,11 @@ module NuestraSuite
   end
 
   def mayor_a(parametro)
-    return proc { |var|  var > parametro }
+    return proc { |var| var > parametro }
   end
 
   def menor_a(parametro)
-    return proc { |var|  var < parametro }
+    return proc { |var| var < parametro }
   end
 
   def uno_de_estos(lista)
@@ -42,21 +42,25 @@ module NuestraSuite
   def method_missing(symbol, *args)
     if symbol.to_s[0..3] == "ser_"
       mensaje = symbol.to_s[4..(symbol.to_s.length-1)] + "?"
-      return proc {|var| var.send(mensaje.to_sym)}
+      return proc { |var| var.send(mensaje.to_sym) }
+
+      elsif symbol.to_s[0..5] == "tener_"
+       mensaje = symbol.to_s[6..(symbol.to_s.length-1)].to_sym
+       return proc { |var| var.instance_variable_get(mensaje).to_sym deberia ser args[0] }
     end
     super
   end
 
 #  def tener(param)
- #   return proc { |var| var.instance_variable_defined? param }
-  #end
+#   return proc { |var| var.instance_variable_defined? param }
+#end
 
   def entender(symbol)
-    return proc { |var|  var.respond_to? symbol }
+    return proc { |var| var.respond_to? symbol }
   end
 
   def explotar_con(exception)
-    return proc {|bloque|
+    return proc { |bloque|
       begin
         bloque.call
         return false
