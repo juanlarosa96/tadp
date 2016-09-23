cea52c52e11a14125b4a3c182c042862e9a26b9b
Juan Larosa <juanlarosa96@hotmail.com>
2016-09-23 17:51:15 -0300

    Agrego clase Espiador

---
+++ Grupo 5 TP TADP/SRC/Espia.rb
@@ -0,0 +1,21 @@
+class Espiador
+
+  def self.new(*args)
+    objeto = args[0].class.new
+
+    #metodos = args[0].class.instance_methods
+
+    metodos = [:sumar, :felicitar]
+    metodos.each { |metodo| objeto.define_singleton_method metodo do |*args|
+         @llamadasAMetodos ||= []
+         @llamadasAMetodos << [metodo].concat(args)
+        printf("Llamada a metodo #{metodo} con argumentos #{args}\n")
+        super(*args)
+      end
+      }
+    objeto.define_singleton_method :llamadasAMetodos do
+    @llamadasAMetodos
+    end
+    return objeto
+    end
+  end
