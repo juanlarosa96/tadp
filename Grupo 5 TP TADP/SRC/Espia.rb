4dd5ef40250e47f763e5ce2f5a171a36aad73af5
Juan Larosa <juanlarosa96@hotmail.com>
2016-09-24 01:52:47 -0300

    No redefino metodo define_singleton_method cuando espio en clase Espiador

--- Grupo 5 TP TADP/SRC/Espia.rb
+++ Grupo 5 TP TADP/SRC/Espia.rb
@@ -1,21 +1,18 @@
 class Espiador
 
   def self.new(*args)
-    objeto = args[0].class.new
+    objeto = args[0]
+    metodos = args[0].methods
+    metodos.delete(:define_singleton_method)
 
-    metodos = args[0].class.instance_methods
-
-    #metodos = [:sumar, :felicitar]
     metodos.each { |metodo| objeto.define_singleton_method metodo do |*args|
          @llamadasAMetodos ||= []
          @llamadasAMetodos << [metodo].concat(args)
-        printf("Llamada a metodo #{metodo.to_s} con argumentos #{args}\n")
+        #printf("Llamada a metodo #{metodo.to_s} con argumentos #{args}\n")
         super(*args)
       end
       }
-    objeto.define_singleton_method :llamadasAMetodos do
-    @llamadasAMetodos
-    end
+    objeto.define_singleton_method :llamadasAMetodos do  return @llamadasAMetodos    end
     return objeto
     end
   end
