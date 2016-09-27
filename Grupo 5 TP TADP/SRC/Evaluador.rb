8eff5461dcebf76eabc9ae13d8dbb98791bffe25
Juan Larosa <juanlarosa96@hotmail.com>
2016-09-26 22:41:11 -0300

    Agrego abstraccion Evaluador para reemplazar los procs

---
+++ Grupo 5 TP TADP/SRC/Evaluador.rb
@@ -0,0 +1,51 @@
+require_relative "../SRC/FalloTest"
+
+class Evaluador
+
+  attr_accessor :proc_comparador, :fallo
+
+  def initialize(comparador, fallo)
+    self.proc_comparador=comparador
+    self.fallo=fallo
+  end
+
+  def self.mayor_a(parametro)
+    new(proc { |var| var > parametro }, FalloTest.new("El objeto no es mayor a #{parametro}"))
+  end
+
+  def self.menor_a(parametro)
+    new(proc { |var| var < parametro }, FalloTest.new("El objeto no es menor a #{parametro}"))
+  end
+
+  def self.uno_de_estos(parametro)
+    new(proc { |var| parametro.include? var }, FalloTest.new("El objeto no es un elemento de la lista "))
+  end
+
+  def self.ser_(mensaje)
+    new(proc { |var| var.send(mensaje.to_sym) }, FalloTest.new("El mensaje #{mensaje} no retorna True"))
+  end
+
+  def self.entender(parametro)
+    new(proc { |var| var.respond_to? parametro }, FalloTest.new("El objeto no entiende el mensaje #{parametro}"))
+  end
+
+  def self.explotar_con(parametro)
+    new(proc { |bloque|
+      begin
+        bloque.call
+        raise FalloTest.new("Se esperaba Excepcion '#{parametro.to_s}' pero no sucedio")
+
+      rescue Exception => e
+        if not(e.is_a? parametro)
+          raise FalloTest.new("Se esperaba Excepcion '#{parametro.to_s}' pero se produjo Excepcion #{e.class.to_s}")
+        end
+      end
+      true
+    }, nil)
+  end
+
+  def call(contexto)
+    raise fallo unless proc_comparador.call contexto
+  end
+
+end
