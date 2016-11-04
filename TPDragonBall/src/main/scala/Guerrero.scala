8751ba1e123aab5fbd8399207744745b28f9ce27
JavierZ <javier.zolotarchuk@despegar.com>
2016-11-04 02:59:28 -0300

    ahora compila ;) y esta implementado el cargar ki y las llamadas a los distintos movimientos

--- TPDragonBall/src/main/scala/Guerrero.scala
+++ TPDragonBall/src/main/scala/Guerrero.scala
@@ -1,62 +1,65 @@
 
 trait Guerrero {
-
   val energiaMaxima: Int
-  var energia: FuenteDeEnergia
-  var movimientos: List[Movimiento]
-  var items: List[Item]
+  val energia: FuenteDeEnergia
+  val movimientos: List[String]
+  val items: List[Item]
 
-  def ejecutar(mov: Movimiento): Guerrero = {
-    mov match {
-      case CargarKi => cargarKi(this)
-    }
+  def ejecutar(mov: Movimiento): Guerrero = { //como vamos a tener muchos movimientos, para esto esta mejor el poli ad-hoc
+    mov.apply(this)
   }
 
-
+  def dejarseFajar = {}
 }
 
 // ------------------------------ TIPOS DE GUERRERO ------------------------------
 
-case class Humano extends Guerrero
-case class Androide extends Guerrero
-case class Namekusein extends Guerrero
-case class Monstruo extends Guerrero
+case class Humano(energiaMaxima: Int, energia: FuenteDeEnergia, movimientos: List[String], items: List[Item]) extends Guerrero
+case class Androide(energiaMaxima: Int, energia: FuenteDeEnergia, movimientos: List[String], items: List[Item]) extends Guerrero
+case class Namekusein(energiaMaxima: Int, energia: FuenteDeEnergia, movimientos: List[String], items: List[Item]) extends Guerrero
+case class Monstruo(energiaMaxima: Int, energia: FuenteDeEnergia, movimientos: List[String], items: List[Item]) extends Guerrero
 
 case class Saiyajin(ki: Int,
                     cola: Boolean,
-                    estado: Estado) extends Guerrero
+                    estado: Estado,
+                    energiaMaxima: Int, energia: FuenteDeEnergia, movimientos: List[String], items: List[Item]) extends Guerrero
 
 case class Fusion(unGuerrero: Guerrero,
                   otroGuerrero: Guerrero) extends Guerrero {
+  var energiaMaxima: Int = null
+  var energia: FuenteDeEnergia = null
+  var movimientos: List[String] = null
+  var items: List[Item] = null
+  def cargarKi = this
 
-  val energiaMaxima = unGuerrero.energiaMaxima + otroGuerrero.energiaMaxima
-  var energia = unGuerrero.energia + otroGuerrero.energia
-  var movimientos = unGuerrero.movimientos ::: otroGuerrero.movimientos
-  var items = unGuerrero.items ::: otroGuerrero.movimientos
+  def apply = {
+    energiaMaxima = unGuerrero.energiaMaxima + otroGuerrero.energiaMaxima
+    energia = Ki(unGuerrero.energia.cant + otroGuerrero.energia.cant)
+    movimientos = unGuerrero.movimientos ::: otroGuerrero.movimientos
+    items = unGuerrero.items ::: otroGuerrero.items
+    this
+  }
 }
 
 
 // ------------------------- ESTADOS SAIYAN --------------------------
 
 trait Estado {
-  def cargarKi(guerrero: Guerrero): Guerrero = {
-    guerrero.energia = guerrero.energia + 100
-    guerrero
-  }
 }
 
-case class SuperSaiyajin(nivel: Int) extends Estado {
-  override def cargarKi(guerrero: Guerrero): Guerrero = {
-    guerrero.energia = guerrero.energia + 150 * nivel
-    guerrero
-  }
-}
+case class SuperSaiyajin(nivel: Int, energiaMaxima: Int, energia: FuenteDeEnergia, movimientos: List[String], items: List[Item]) extends Estado with Guerrero
+
 case object Mono extends Estado
 case object Normal extends Estado
 
 // ------------------------- FUENTES DE ENERGIA --------------------------
-
-abstract class FuenteDeEnergia
+abstract class  FuenteDeEnergia {
+  abstract def cant :Int
+}
 
 case class Ki(cant: Int) extends FuenteDeEnergia
+
+
 case class Bateria(cant: Int) extends FuenteDeEnergia
+
+
--- TPDragonBall/src/main/scala/Movimiento.scala
+++ TPDragonBall/src/main/scala/Movimiento.scala
@@ -1,7 +1,20 @@
 /**
   * Created by gas on 27/10/16.
   */
-abstract class Movimiento
+trait Movimiento {
+  abstract def apply(guerrero :Guerrero) : Guerrero
+}
 
-case object CargarKi extends Movimiento
+case object CargarKi extends Movimiento {
+  def apply(guerrero :Guerrero) :Guerrero = {
+    guerrero match {
+      case humano :Humano => humano.copy(energia = Ki(humano.energia.cant +100))
+      case superSaiyajin :SuperSaiyajin => superSaiyajin.copy(energia = Ki(superSaiyajin.energia.cant +150 * superSaiyajin.nivel))
+      case namekusein :Namekusein => namekusein.copy(energia = Ki(namekusein.energia.cant +100))
+      case monstruo :Monstruo => monstruo.copy(energia = Ki(monstruo.energia.cant +100))
+      case saiyajin :Saiyajin => saiyajin.copy(energia = Ki(saiyajin.energia.cant +100))
+      case _  => _
+    }
+  }
+}
 case object DejarseFajar extends Movimiento
